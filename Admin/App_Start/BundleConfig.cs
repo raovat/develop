efc18cd2f22427abf6f5d8f195e79e7f950a8fb9
minhdv <minhdv85@gmail.com>
2014-04-04 00:19:37 +0700

    add new admin project

---
+++ Admin/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Admin
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254726
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+                  "~/Scripts/WebForms/WebForms.js",
+                  "~/Scripts/WebForms/WebUIValidation.js",
+                  "~/Scripts/WebForms/MenuStandards.js",
+                  "~/Scripts/WebForms/Focus.js",
+                  "~/Scripts/WebForms/GridView.js",
+                  "~/Scripts/WebForms/DetailsView.js",
+                  "~/Scripts/WebForms/TreeView.js",
+                  "~/Scripts/WebForms/WebParts.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+
+            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+            //Plugin bootstrap
+            bundles.Add(new ScriptBundle("~/layout/admin")
+                .Include("~/Layout/js/jquery-1.10.2.js")
+                .Include("~/Layout/js/bootstrap.js")
+                .Include("~/Layout/js/bootstrap.min.js")
+                .Include("~/Layout/js/sb-admin.js")
+                .Include("~/Layout/js/plugins/metisMenu/jquery.metisMenu.js")
+                .Include("~/Layout/js/plugins/morris/raphael-2.1.0.min.js")
+                .Include("~/Layout/js/plugins/morris/morris.js")
+                .Include("~/Layout/js/demo/dashboard-demo.js")
+                );
+        }
+    }
+}
